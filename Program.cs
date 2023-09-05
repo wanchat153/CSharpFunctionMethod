cf16c6d67dfb68891dd00d017dab2b65ab707be8
M153 <wanchatgame@gmail.com>
2023-09-05 13:58:13 +0700

    Function

--- Program.cs
+++ Program.cs
@@ -1,8 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+
+/*
+int i = int.Parse(10);
+
+Access Specifier = กำหนดขอบเขตการทำงานของฟังก์ชัน เช่น int
+Returb Type = ค่าที่จะคืนกลับมา เช่น Parse
+Method Name = ชื่อของ Function, Method, Variable(ตัวแปร หรือ var ตั้งให้สื่อความหมาย)
+Parameter List = กำหนดว่า รับข้อมูลอะไรเข้าไปประมวลผลได้บ้าง เช่น int.Parse("10");
+Method Body = code การทำงาน
+
+<Access Specifier><Returb Type><Method Name>(Parameter List)
+{
+    Method Body
+}
+*/
 
 namespace CSharpFunctionMethod
 {
